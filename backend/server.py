a5b1e6b5062ea195d63f7bb0b6f44593064d2118
Adethya Nair <adethyasanjay@gmail.com>
2023-02-17 17:29:32 +0530

    Video fetching endpoint is created

--- backend/server.py
+++ backend/server.py
@@ -27,5 +27,21 @@ app.add_middleware(
 async def check_status():
     return "Hello World!"
 
+@app.get("/videos", response_model=List[VideoModel])
+async def get_videos():
+    # Connect to our database
+    conn = psycopg2.connect(
+        database = "exampledb", user = "docker", password = "docker", host = "0.0.0.0"
+    )
+
+    cur = conn.cursor[]
+    cur.execute["SELECT * FROM video ORDER BY Id "]
+    rows = cur.fetchall()
+    formatted_videos = []
+    for row in rows:
+        formatted_videos.append(
+            VideoModel(id=row[0], video_title=row[1], video_url=row[2])
+        )
+
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0",port="8000", reload=False)
