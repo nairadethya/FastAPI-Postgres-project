04df8e6884dece88440ded42b1890fa0d3b079be
Adethya Nair <adethyasanjay@gmail.com>
2023-02-17 14:33:13 +0530

    Error browser not responding is resolved

--- backend/server.py
+++ backend/server.py
@@ -16,4 +16,4 @@ async def check_status():
     return "Hello World!"
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=False)
+    uvicorn.run(app, host="0.0.0.0",port="8000", reload=False)
