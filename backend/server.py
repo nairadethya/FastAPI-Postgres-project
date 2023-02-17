6f718632c70036fb7908a2ca15474fda2d58b054
Adethya Nair <adethyasanjay@gmail.com>
2023-02-17 13:11:27 +0530

    A basic view is created

---
+++ backend/server.py
@@ -0,0 +1,19 @@
+import boto3
+import psycopg2
+
+from typing import List
+from pydantic import BaseModel
+
+import uvicorn
+from fastapi import FastAPI, UploadFile
+
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(debug=True)
+
+@app.get("/status")
+async def check_status():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=False)
