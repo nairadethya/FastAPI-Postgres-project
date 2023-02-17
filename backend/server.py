3557cfec2f1c3ac9e9520f38c568d66b32d3646e
Adethya Nair <adethyasanjay@gmail.com>
2023-02-17 16:58:59 +0530

    Middleware is created

--- backend/server.py
+++ backend/server.py
@@ -9,7 +9,19 @@ from fastapi import FastAPI, UploadFile
 
 from fastapi.middleware.cors import CORSMiddleware
 
+class VideoModel(BaseModel):
+    id: int
+    video_title: str
+    video_url: str
+
 app = FastAPI(debug=True)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["*"],
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"],
+)
 
 @app.get("/status")
 async def check_status():
